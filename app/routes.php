b0b77c0df7434acf0e32e69bb7ddf892610a7683
Stella Kim <stella.kim@icloud.com>
2014-04-07 16:37:56 -0400

    route to get user model data

--- app/routes.php
+++ app/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', 'PagesController@home');
 
 Route::get('/about', 'PagesController@about');
+
+Route::get('users', function() 
+{
+	$users = User::all(); // select * from users
+
+	$user = User::find(1);
+	return $user->email;
+});
