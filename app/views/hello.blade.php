3250c52974f417109741cef7516298eba37a84b9
Stella Kim <stella.kim@icloud.com>
2014-04-22 17:01:28 -0400

    changing template engine to twig

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,9 +1,10 @@
-@extends('layouts.default')
-@section('content')
+{% extends 'layouts/default.html' %}
+
+{% block content %}
 		<h1>Home</h1>
 
 		<ul>
 			<li>{{ link_to("/users/", "Users") }}</li>
 			<li>{{ link_to("/about/", "About") }}</li>
 		</ul>
-@stop
+{% endblock %}
